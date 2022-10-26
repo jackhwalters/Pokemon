11f62feb732419d87a42b75e5c4c540559190adf
Jack Walters <jackwalterswork@gmail.com>
2022-10-26 17:12:11 +0100

    Develop Endpoint 1

--- api/routes/pokemon.js
+++ api/routes/pokemon.js
@@ -1,19 +1,41 @@
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 const router = express();
 
 
+// Endpoint for Endpoint 1
 router.get("/:pokemonName", async (req, res) => {
-    console.log("pokemon name: ", req.params.pokemonName);
-    
-    res.status(200).json({
-        name: "mewtwo",
-        description: "It was created by a scientist after years of horrific gene splicing " +
-        "and DNA engineering experiments.",
-        habitat: "rare",
-        isLegendary: true
-    });
+    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${req.params.pokemonName}`)
+        .then(response => {
+            const name = ('name' in response.data) ? response.data.name : "undefined";
+            const description = ('flavor_text_entries' in response.data) ? response.data.flavor_text_entries[0].flavor_text : "undefined";
+            const habitat = ('habitat.name' in response.data) ? response.data.habitat.name : "undefined"
+            const isLegendary = ('is_legendary' in response.data) ? response.data.is_legendary : "undefined"
+
+            return res.status(200).json({
+                name: name,
+                description: description,
+                habitat: habitat,
+                isLegendary: isLegendary
+            });
+        })
+        .catch(error => {
+            if (error.response || error.request) {
+                // Simplistic error returning - verbosity depends on where this API will be used
+                console.log("Error: ", error);
+                res.status(error.response.status).json({
+                    message: `Error: ${error.response.statusText}`
+                });
+            }
+            else {
+                res.status(500).json({
+                    message: "An unknown error has occured"
+                });
+            }
+        });
 });
 
+// Endpoint for Endpoint 2
 router.get("/translated/:pokemonName", async (req, res) => {
     console.log("pokemon name for translated:", req.params.pokemonName);
 
@@ -26,4 +48,4 @@ router.get("/translated/:pokemonName", async (req, res) => {
     });
 })
 
-module.exports = router; // enable exporting of routes
+module.exports = router; // Enable exporting of routes
--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const app = express();
 // Import routes files
 const pokemon = require('./api/routes/pokemon');
 
-// set up the routes
+// Set up the routes
 app.use("/pokemon", pokemon);
 
-// error handling for every request that reaches this line
+// Error handling for every request that reaches this line
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
     next(error);
 });
 
-// handle errors thrown from anywhere else in the application
+// Handle errors thrown from anywhere else in the application
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
@@ -24,4 +24,4 @@ app.use((error, req, res, next) => {
     });
 });
 
-module.exports = app; // enable exporting of routes
+module.exports = app; // Enable exporting of routes
